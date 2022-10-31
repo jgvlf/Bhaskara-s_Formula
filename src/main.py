1e9489763895aae4d527ff70419c189dd7cee6e7
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-10-31 08:42:13 -0300

    👌 IMPROVE: Add Input Values

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
 from math import pow, sqrt
 from sys import exit
+
+a = int(input("Digite o valor do coeficiente \"a\": "))
+b = int(input("Digite o valor do coeficiente \"b\": "))
+c = int(input("Digite o valor do coeficiente \"c\": "))
+
