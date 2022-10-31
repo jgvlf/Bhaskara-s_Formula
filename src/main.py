acc60e4076c7b7b46456c3e0f674b0d7e690360a
joaogabrielvlf <joaogabrielvlf@gmail.com>
2022-10-31 08:42:41 -0300

    👌 IMPROVE: Verify Crescent or Decrescent Graphic.

--- src/main.py
+++ src/main.py
@@ -5,3 +5,7 @@ a = int(input("Digite o valor do coeficiente \"a\": "))
 b = int(input("Digite o valor do coeficiente \"b\": "))
 c = int(input("Digite o valor do coeficiente \"c\": "))
 
+if(a < 0):
+    ordem_gráfico = "O gráfico é decresente."
+else:
+    ordem_gráfico = "O gráfico é crescente."
